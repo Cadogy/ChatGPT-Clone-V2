f914170b80757719906cf1e35c4f157eb17ac436
Ross Cohen <rosscohen1990@gmail.com>
2023-02-17 00:04:42 -0500

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCZBJROhMfydpSY5PpthYdqBVzjZ2JeN38",
-  authDomain: "gpt-portal-377702.firebaseapp.com",
-  projectId: "gpt-portal-377702",
-  storageBucket: "gpt-portal-377702.appspot.com",
-  messagingSenderId: "195278515475",
-  appId: "1:195278515475:web:1b6c3a100501e19d8bfee6",
-  measurementId: "G-T03W86J7XG"
+  apiKey: "AIzaSyDIHu44aJy4h4PJtxMG-vFZg-al7T1JRsM",
+  authDomain: "chatgpt-84d8f.firebaseapp.com",
+  projectId: "chatgpt-84d8f",
+  storageBucket: "chatgpt-84d8f.appspot.com",
+  messagingSenderId: "328919260204",
+  appId: "1:328919260204:web:59dcc812e3cfebb3738589",
+  measurementId: "G-B5BSGEX2HJ"
 };
 
 // Initialize Firebase
